a8fa704c7c56eb742d344a6c246d8ea9e6362a1b
Abhay Vaidya <abhay6547@gmail.com>
2017-12-25 16:21:02 -0500

    Added man, umbrella, and collision

--- drop.js
+++ drop.js
@@ -1,54 +1,48 @@
-function Drop(){
-  this.x = random(width);
-  this.y = random(-500, 50);
-  this.z = random(0,20);
-  this.explode = false
-  this.len = map(this.z, 0, 20, 10, 20);
-  this.yspeed = map(this.z, 0, 20, 1, 20);
-
-  this.fall = function(){
-    this.y = this.y + this.yspeed;
-    var gravity = map(this.z, 0, 20, 0.1, 0.2);
-    this.yspeed = this.yspeed + gravity;
-
-
-    if ((this.y > height)||this.touching()){
-      ellipse(posx, posy, 20, 5);
-      for (var i = 0; i < 10; i++){
-        var yrandom = random(30,150);
-        var posx = random(this.x-10, this.x+10);
-        var posy = random(this.y, this.y-yrandom);
-        noStroke();
-        fill(255, 255, 255, 50);
-        ellipse(posx, posy, random(1,5), random(1,5));
-      }
-
-      for (var i = 0; i < 2; i++){
-        var yrandom = random(20,50);
-        var posx = random(this.x-5, this.x+5);
-        var posy = random(this.y, this.y-yrandom);
-        noStroke();
-        fill(255, 255, 255, 50);
-        ellipse(posx, posy, random(2,5), random(2,5));
-      }
-
-      this.y = random(-200, -100);
-      this.yspeed = map(this.z, 0, 20, 4, 10);
+class Drop {
+    constructor() {
+        this.x = random(width);
+        this.y = random(-500, 50);
+        this.z = random(0, 20);
+        this.len = map(this.z, 0, 20, 10, 20);
+        this.yspeed = map(this.z, 0, 20, 1, 20);
     }
-  }
-
-  this.touching = function(){
-    if(this.x == mouseX && this.y == mouseY){
-      return true;
-    }else{
-      return false;
+    
+    fall() {
+        this.y = this.y + this.yspeed;
+        var gravity = map(this.z, 0, 20, 0.1, 0.2);
+        this.yspeed = this.yspeed + gravity;
+        if ((this.y > height) || this.touching()) {
+            ellipse(posx, posy, 20, 5);
+            for (var i = 0; i < 10; i++) {
+                var yrandom = random(30, 150);
+                var posx = random(this.x + 10, this.x + 30);
+                var posy = random(this.y, this.y - yrandom);
+                noStroke();
+                fill(255, 255, 255, 50);
+                ellipse(posx, posy, random(1, 5), random(1, 5));
+            }
+            for (var i = 0; i < 2; i++) {
+                var yrandom = random(20, 50);
+                var posx = random(this.x + 5, this.x + 10);
+                var posy = random(this.y, this.y - yrandom);
+                noStroke();
+                fill(255, 255, 255, 50);
+                ellipse(posx, posy, random(2, 5), random(2, 5));
+            }
+            this.y = random(-200, -100);
+            this.yspeed = map(this.z, 0, 20, 4, 10);
+        }
     }
-  }
-
-  this.show = function(){
-    var thick = map(this.z, 0, 20, 1, 4);
-    strokeWeight(thick);
-    stroke(92, 113, 134,75);
-    line(this.x,this.y,this.x,this.y+this.len);
-  }
+    
+    touching() {
+        var d = dist(this.x, this.y, mouseX, mouseY);
+        return (d < 95);
+    }
+    
+    show() {
+        var thickness = map(this.z, 0, 20, 1, 4);
+        strokeWeight(thickness);
+        stroke(92, 113, 134, 75);
+        line(this.x, this.y, this.x, this.y + this.len);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,34 @@
   var drops = []
-  var numDrops = 600;
+  var numDrops = 700;
 
-function setup(){
-  createCanvas(640, 360);
-
-  for (var i = 0; i < numDrops; i++){
-    drops[i] = new Drop()
-  }
-}
-
-function draw() {
-  lightning = random(1,100);
-  if(lightning < 99.9){
-    background(44, 62, 80);
-  } else{
-    background(199, 207, 219);
+  function setup() {
+      createCanvas(windowWidth, windowHeight);
+      for (var i = 0; i < numDrops; i++) {
+          drops[i] = new Drop();
+      }
+      umbrella = loadImage("umbrella.png");
+      man = loadImage("man.png");
   }
 
-  rotate(0.025);
-  for(var i = 0; i < drops.length; i++){
-    drops[i].fall();
-    drops[i].show();
+  function draw() {
+      lightning = random(1, 100);
+      if (lightning < 99.9) {
+          background(14, 32, 50);
+      }
+      else {
+          background(199, 207, 219);
+      }
+      
+      imageMode(CENTER);
+      image(man, windowWidth/2, windowHeight - man.height/4 + 10, man.width/2, man.height/2);
+      image(umbrella, mouseX, mouseY, umbrella.width/3, umbrella.height/3);
+      
+      for (var i = 0; i < drops.length; i++) {
+          drops[i].fall();
+          drops[i].show();
+      }
   }
-}
+
+  function windowResized() {
+      resizeCanvas(windowWidth, windowHeight);
+  }
